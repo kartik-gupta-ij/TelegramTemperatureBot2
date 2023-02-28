0f599d3034d661320b9af7e5873f5ddaaf0e04a3
Kartik Gupta <kartikgupta7267@gmail.com>
2023-02-28 12:51:27 +0530

    rr

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3001, () => {
-  console.log('Server listening on port 3001');
+app.listen(80, () => {
+  console.log('Server listening on port 80');
 });
